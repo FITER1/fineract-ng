20e7f54355c785ee2b1c2de268b9f3b274555df6
Aleksandar Vidakovic <aleks@fiter.io>
2019-10-22 17:23:40 +0200

    Enable legacy authentication again

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/boot/SecurityBasicConfiguration.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/boot/SecurityBasicConfiguration.java
@@ -50,12 +50,12 @@ public class SecurityBasicConfiguration extends WebSecurityConfigurerAdapter {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()
-                // .antMatchers("/api/**/authentication").permitAll()
-                // .antMatchers("/api/**/self/authentication").permitAll()
-                // .antMatchers("/api/**/self/registration").permitAll()
-                // .antMatchers("/api/**/self/user").permitAll()
-                .antMatchers("/api/**").fullyAuthenticated()
-                // .antMatchers("/api/**").hasAuthority("TWOFACTOR_AUTHENTICATED")
+            .antMatchers("/api/**/authentication").permitAll()
+            .antMatchers("/api/**/self/authentication").permitAll()
+            .antMatchers("/api/**/self/registration").permitAll()
+            .antMatchers("/api/**/self/user").permitAll()
+            .antMatchers("/api/**").fullyAuthenticated()
+            // .antMatchers("/api/**").hasAuthority("TWOFACTOR_AUTHENTICATED")
         ;
     }
 
